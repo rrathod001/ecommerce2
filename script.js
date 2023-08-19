117e85247ed5f706260dcf73f88309ed35ef28c5
Ritesh Rathod <126505805+rrathod001@users.noreply.github.com>
2023-08-19 12:13:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ let hamb = document.getElementById('hamb')
 
 let carous = document.getElementById('carous')
 
+let aside1 = document.getElementById('aside_down111')
+let aside2 = document.getElementById('aside_down222')
+let aside3 = document.getElementById('aside_down333')
+let aside4 = document.getElementById('aside_down444')
+let aside5 = document.getElementById('aside_down555')
+
 
 document.addEventListener('click', function(event) {
     var clickedElement = event.target;
@@ -134,6 +140,76 @@ document.addEventListener('click', function(event) {
         item3.classList.add('d-none')
         carous.classList.add('carousel-inner-zin')
     }
+    else if (clickedElement.id === 'aside_down1') {
+      aside1.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside4.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down11') {
+      aside1.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside4.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down2') {
+      aside2.classList.toggle('d-none')
+      aside1.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside4.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down22') {
+      aside2.classList.toggle('d-none')
+      aside1.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside4.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down3') {
+      aside3.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside1.classList.add('d-none')
+      aside4.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down33') {
+      aside3.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside1.classList.add('d-none')
+      aside4.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down4') {
+      aside4.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside1.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down44') {
+      aside4.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside1.classList.add('d-none')
+      aside5.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down5') {
+      aside5.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside1.classList.add('d-none')
+      aside4.classList.add('d-none')
+    }
+    else if (clickedElement.id === 'aside_down55') {
+      aside5.classList.toggle('d-none')
+      aside2.classList.add('d-none')
+      aside3.classList.add('d-none')
+      aside1.classList.add('d-none')
+      aside4.classList.add('d-none')
+    }
     else
     {
         ws.classList.add('d-none')
@@ -142,6 +218,11 @@ document.addEventListener('click', function(event) {
         item3.classList.add('d-none')
         item4.classList.add('d-none')
         carous.classList.remove('carousel-inner-zin')
+        aside2.classList.add('d-none')
+        aside3.classList.add('d-none')
+        aside1.classList.add('d-none')
+        aside4.classList.add('d-none')
+        aside5.classList.add('d-none')
     }
   });
 
